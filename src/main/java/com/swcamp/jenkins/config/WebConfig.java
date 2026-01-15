218b2da06aca9517ef5258fbf5cf8b50212ace94
fijitlwkr <hakyung1133@gmail.com>
2026-01-15 12:45:35 +0900

    WebConfig 수정

--- src/main/java/com/swcamp/jenkins/config/WebConfig.java
+++ src/main/java/com/swcamp/jenkins/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                /* NodePort에 설정된 30000번 포트에서 오는 연결에 대한 CORS설정 해제 */
-                .allowedOrigins("http://localhost:30000")
+                .allowedOrigins("")
                 .allowedMethods("GET",  "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 }
